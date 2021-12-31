0b5a3211ab1ff87cdcc258ad1b0b4d33f1fbfb38
Scott A. Hyslop <sahyslop@gmail.com>
2021-12-30 20:19:12 -0500

    Initial Angular13 project creation, basic layout organized and primitive nav constructed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { DevicesComponent } from './components/devices/devices.component';
+import { DeviceComponent } from './components/device/device.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    DevicesComponent,
+    DeviceComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
