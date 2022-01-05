6ce63afd45dd861a72d8051e1ae5974d37ec5258
Scott A. Hyslop <sahyslop@gmail.com>
2022-01-05 13:32:22 -0500

    Consigned some scrap to the code graveyard in devices.component.ts

--- src/app/components/devices/devices.component.ts
+++ src/app/components/devices/devices.component.ts
@@ -61,34 +61,12 @@ export class DevicesComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.getDeviceOSAndTypeIcons();
+   
 
   }//end ngOnInit()
 
 
-    getDeviceOSAndTypeIcons()
-    {
-      //load up OS Icons with types
 
-      // const osIcons:any[][]
-      var osIcons = this.getOSIcons();
-
-      //load up Device Icons with types
-      var deviceIcons = this.getDeviceIcons();
-
-    }//end getDeviceOSAndTypeIcons
-
-     getOSIcons(){
-      const osIcons:any[][] = [];
-      var deviceOS = ["iOS", "Windows", "Android"];
-      var deviceOSIconPath = [ "<svg aria-hidden='true' focusable='false' data-prefix='fab' data-icon='apple' class='svg-inline--fa fa-apple' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512'><path fill='currentColor' d='M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z'></path></svg>", "   <svg aria-hidden='true' focusable='false' data-prefix='fab' data-icon='windows' class='svg-inline--fa fa-windows' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'><'path fill='currentColor' d='M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6 25.3V268.4H0v149.9zm203.8 28L448 480V268.4H203.8v177.9zm0-380.6v180.1H448V32L203.8 65.7z'></path></svg>", "<svg aria-hidden='true' focusable='false' data-prefix='fab' data-icon='android' class='svg-inline--fa fa-android' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512'><path fill='currentColor' d='M420.5 301.9a24 24 0 1 1 24-24 24 24 0 0 1 -24 24m-265.1 0a24 24 0 1 1 24-24 24 24 0 0 1 -24 24m273.7-144.5 47.94-83a10 10 0 1 0 -17.27-10h0l-48.54 84.07a301.3 301.3 0 0 0 -246.6 0L116.2 64.45a10 10 0 1 0 -17.27 10h0l47.94 83C64.53 202.2 8.24 285.5 0 384H576c-8.24-98.45-64.54-181.8-146.9-226.6'></path></svg>"];
-
-      for (var [index, icon] of deviceOS.entries()) {
-        osIcons[index].push(deviceOS[index], deviceOSIconPath[index]);
-        }
-
-        return osIcons;
-    }//end getOSIcons
 
     getDeviceIcons(){
       const deviceIcons:any[][] = [];
@@ -125,3 +103,28 @@ export class DevicesComponent implements OnInit {
 
       }
  */
+/*
+ getDeviceOSAndTypeIcons()
+    {
+      //load up OS Icons with types
+
+      // const osIcons:any[][]
+      var osIcons = this.getOSIcons();
+
+      //load up Device Icons with types
+      var deviceIcons = this.getDeviceIcons();
+
+    }//end getDeviceOSAndTypeIcons
+
+     getOSIcons(){
+      const osIcons:any[][] = [];
+      var deviceOS = ["iOS", "Windows", "Android"];
+      var deviceOSIconPath = [ "<svg aria-hidden='true' focusable='false' data-prefix='fab' data-icon='apple' class='svg-inline--fa fa-apple' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512'><path fill='currentColor' d='M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z'></path></svg>", "   <svg aria-hidden='true' focusable='false' data-prefix='fab' data-icon='windows' class='svg-inline--fa fa-windows' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'><'path fill='currentColor' d='M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6 25.3V268.4H0v149.9zm203.8 28L448 480V268.4H203.8v177.9zm0-380.6v180.1H448V32L203.8 65.7z'></path></svg>", "<svg aria-hidden='true' focusable='false' data-prefix='fab' data-icon='android' class='svg-inline--fa fa-android' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512'><path fill='currentColor' d='M420.5 301.9a24 24 0 1 1 24-24 24 24 0 0 1 -24 24m-265.1 0a24 24 0 1 1 24-24 24 24 0 0 1 -24 24m273.7-144.5 47.94-83a10 10 0 1 0 -17.27-10h0l-48.54 84.07a301.3 301.3 0 0 0 -246.6 0L116.2 64.45a10 10 0 1 0 -17.27 10h0l47.94 83C64.53 202.2 8.24 285.5 0 384H576c-8.24-98.45-64.54-181.8-146.9-226.6'></path></svg>"];
+
+      for (var [index, icon] of deviceOS.entries()) {
+        osIcons[index].push(deviceOS[index], deviceOSIconPath[index]);
+        }
+
+        return osIcons;
+    }//end getOSIcons
+*/
